b2e936730c9fe2fc29e1fd9ecdce81124c4dcfcc
root <gitlab_admin_f1e841@example.com>
2025-03-19 09:40:48 +0530

    cgnage

--- nb-dt-import.py
+++ nb-dt-import.py
@@ -17,7 +17,7 @@ def main():
     netbox = NetBox(settings)
     files, vendors = settings.dtl_repo.get_devices(
         f'{settings.dtl_repo.repo_path}/device-types/', args.vendors)
-
+    settings.handle.log(f"Files found in device-types: {files}")
     settings.handle.log(f'{len(vendors)} Vendors Found')
     device_types = settings.dtl_repo.parse_files(files, slugs=args.slugs)
     settings.handle.log(f'{len(device_types)} Device-Types Found')
@@ -28,6 +28,7 @@ def main():
         settings.handle.log("Modules Enabled. Creating Modules...")
         files, vendors = settings.dtl_repo.get_devices(
             f'{settings.dtl_repo.repo_path}/module-types/', args.vendors)
+        settings.handle.log(f"Files found in module-types: {files}")
         settings.handle.log(f'{len(vendors)} Module Vendors Found')
         module_types = settings.dtl_repo.parse_files(files, slugs=args.slugs)
         settings.handle.log(f'{len(module_types)} Module-Types Found')
